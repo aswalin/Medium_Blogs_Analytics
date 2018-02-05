eeeeb281043efb355aa6a5e7bf1a5baa556c0f93
Alvira Swalin <alvira@Alvira.local>
2018-02-05 14:09:57 -0800

    blog_second

--- blog.py
+++ blog.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import ast
+from datetime import datetime
 
 r  = requests.get("https://towardsdatascience.com/how-to-handle-missing-data-8646b18db0d4")
 data = r.text
@@ -8,9 +10,13 @@ soup = BeautifulSoup(data)
 title = soup.title.text[0:-23]
 claps = soup.find('button', class_="button button--chromeless u-baseColor--buttonNormal js-multirecommendCountButton").text
 para = [hit.text.strip() for hit in soup.find_all('p')] # this contains linkedin id and description as well
-tags = [hit.text.strip() for hit in soup.find_all('a', class_="link u-baseColor--link")]
+tags = ast.literal_eval(soup.find('script').text)["keywords"]
 bullets = [hit.text.strip() for hit in soup.select('li[class*="graf graf--li"]')]
 images = soup.find_all('img', class_="graf-image")
+format = "%Y-%m-%d"
+datePublished =  datetime.strptime(ast.literal_eval(soup.find('script').text)["datePublished"][0:10], format)
+days_passed = (datetime.now() - datePublished).days
+
 
 
 
